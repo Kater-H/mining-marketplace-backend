ab692fe464d395598ea41d638ff2fa76cbc95d14
Kater-H <haagakater@gmail.com>
2025-06-22 20:39:34 +0100

    Revert "Initial commit: Complete Mining Marketplace backend"
    This reverts commit dde519d34ada4d248ce65fb8003286a286cfae7f.

--- src/__tests__/minimal-test.ts
+++
@@ -1,9 +0,0 @@
-import { HealthController } from '../controllers/healthController';
-
-describe('Minimal Import Test', () => {
-  it('should be able to import HealthController', () => {
-    // Simply test that the import works
-    const controller = new HealthController();
-    expect(controller).toBeDefined();
-  });
-});
--- src/types/flutterwave-node-v3.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'flutterwave-node-v3' {
-  export default class Flutterwave {
-    constructor(publicKey: string, secretKey: string);
-    
-    Payments: {
-      initialize(payload: any): Promise<any>;
-    };
-  }
-}
